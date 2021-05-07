ad9d0932e2560a2e64b202e5be8920b7a417e560
tabbycatty <thetabbycatty@outlook.com>
2021-05-07 00:44:53 -0300

    Added basic message listening

--- main.js
+++ main.js
@@ -18,3 +18,25 @@ client.on('ready', () => {
     console.log(`Logged in as ${client.user.tag}!`);
 });
 
+// Set default prefix (TODO: read prefix from file and actually use it)
+let prefix = '$'
+
+// Listen for messages
+client.on('message', msg => {
+	// Ignore bots and messages not using the prefix
+	if (!message.content.startsWith(prefix) || message.author.bot) return;
+
+	// Split message into command and arguments
+	const args = message.content.slice(prefix.length).trim().split(' ');
+	const command = args.shift().toLowerCase();
+
+	switch(command){
+		case 'deepfry':
+			msg.channel.send('Deepfry Test')
+			msg.channel.send(args);
+		break;
+	}
+});
+
+// Login with secret from file
+client.login(loginSecret);
